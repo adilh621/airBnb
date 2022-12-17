f1b3bfdca243ceac76450777d3367fd0372eb493
Adil Hussain <adil@adils-mbp.myfiosgateway.com>
2022-12-16 19:24:06 -0500

    frontend is finished

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer(){
+    return(
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-500">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How AirBnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>AirBnb Plus</p>
+                <p>AirBnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Adil Hussain</p>
+                <p>Presents</p>
+                <p>Fake</p>
+                <p>AirBnb</p>
+                
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Trust & Safety</p>
+                <p>Say Hello</p>
+                <p>Easter Eggs</p>
+                <p>For the Win</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>This is NOT a real site</p>
+                <p>Explore</p>
+                <p>Airbnb Clone</p>
+                <p>Open to opportunities</p>
+                <p>Follow me on linkedin</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Footer
